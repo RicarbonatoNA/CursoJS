4296a4fd4777c99407caf576d19470b12bf07eb6
Ricardo Gonzalez <gonzalez.ricardo.1bm@gmail.com>
2023-09-15 12:51:03 -0600

    validando cada campo

--- Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
-
     //Seleccionar los elementos de la interfaz
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
 
     //Asignar eventos
     inputEmail.addEventListener('blur', validar);
@@ -11,10 +11,21 @@ document.addEventListener('DOMContentLoaded', function(){
     inputMensaje.addEventListener('blur', validar);
 
     function validar(e){
+        console.log(e.target.id);
         if (e.target.value.trim() === '') {
-            console.log('está vacio');
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`);
         } else {
             console.log('si hay algo...');
         }
+    }
+
+    function mostrarAlerta(mensaje){
+        //Generar alerta con HTML
+        const error = document.createElement('P');
+        error.textContent = mensaje;
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
+
+        //Inyectar el error al formulario
+        formulario.appendChild(error);
     }
 });
