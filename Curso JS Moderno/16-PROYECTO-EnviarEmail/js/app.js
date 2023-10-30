c0c2a34cd81e6656a8038f521fc92d2045209eab
Ricardo Gonzalez <gonzalez.ricardo.1bm@gmail.com>
2023-10-30 15:51:06 -0600

    avance curso

--- Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -1,29 +1,64 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const email = {
+        email:'',
+        asunto:'',
+        mensaje:''
+    }
+
+    console.log(email); 
+
     //Seleccionar los elementos de la interfaz
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]');
 
     //Asignar eventos
-    inputEmail.addEventListener('blur', validar);
-    inputAsunto.addEventListener('blur', validar);
-    inputMensaje.addEventListener('blur', validar);
+    inputEmail.addEventListener('input  ', validar);
+    inputAsunto.addEventListener('input  ', validar);
+    inputMensaje.addEventListener('input  ', validar);
+
+    btnReset.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        //Reiniciar el objeto
+        email.email = '';
+        email.asunto = '';
+        email.mensaje = '';
+
+        formulario.reset();
+        comprobarEmail();
+    })
 
     function validar(e){
         if (e.target.value.trim() === '') {
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
-        } else {
-            console.log('si hay algo...');
+            email[e.target.name] = '';
+            comprobarEmail();
+            return;
         }
+
+        if(e.target.id === 'email' && !validarEmail(e.target.value)){
+            mostrarAlerta('El email no es valido', e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
+            return;
+        }
+
+        limpiarAlerta(e.target.parentElement);
+        
+        //Asignar los valores
+        email[e.target.name] = e.target.value.trim().toLowerCase();
+        
+        //Comprobar el objeto de email
+        comprobarEmail()
     }
 
     function mostrarAlerta(mensaje, referencia){
-        //Comprueba si ya existe una alerta
-        const alerta = referencia.querySelector('.bg-red-600');
-        if (alerta) {
-            alerta.remove();
-        }
+        limpiarAlerta(referencia);
+
         //Generar alerta con HTML
         const error = document.createElement('P');
         error.textContent = mensaje;
@@ -31,5 +66,29 @@ document.addEventListener('DOMContentLoaded', function(){
 
         //Inyectar el error al formulario
         referencia.appendChild(error);
+    }
+
+    function limpiarAlerta(referencia){
+        //Comprueba si ya existe una alerta
+        const alerta = referencia.querySelector('.bg-red-600');
+        if (alerta) {
+            alerta.remove();
+        }
+    }
+
+    function validarEmail(email){
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
+        const resultado = regex.test(email);
+        return resultado;
+    }
+
+    function comprobarEmail() {
+        if (Object.values(email).includes('')) {
+            btnSubmit.classList.add('opacity-50');
+            btnSubmit.disabled = true;
+            return
+        }
+        btnSubmit.classList.remove('opacity-50');
+        btnSubmit.disabled = false;
     }
 });
